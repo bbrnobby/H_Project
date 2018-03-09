e066263f1c7ab4ab75b4344c27ba738c953cff87
Nobby <bbrnobby@gmail.com>
2018-03-09 15:05:25 +0900

    add sceneloader

---
+++ H_Project/Assets/Scripts/Result/ChangeToTitle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  //シーン遷移用
+
+public class ChangeToTitle : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+}
